b22abfe13bd9fc373629d4a27095eb7261914d76
Morro57 <cristian.morrone@alessandrinimainardi.edu.it>
2025-10-20 13:47:36 +0200

    aggiornato parametro allowed host

--- primo_progetto/settings.py
+++ primo_progetto/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-36t88b%zi1%0vf_%vrq%!@0ocgr6lmb_*y4d#isnfro*j22sp_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
